fa093a8c2a404f33b88facd4c7e82be0cf6079b5
Miraoui Jamel <105128037+Jamel-miraoui@users.noreply.github.com>
2023-11-30 17:05:58 +0100

    last upadate

---
+++ app/src/main/java/com/example/alarm/AlarmAdapter.java
@@ -0,0 +1,57 @@
+package com.example.alarm;// AlarmAdapter.java
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.alarm.Alarm;
+
+import java.util.List;
+
+public class AlarmAdapter extends ArrayAdapter<Alarm> {
+
+    public AlarmAdapter(@NonNull Context context, int resource, @NonNull List<Alarm> alarms) {
+        super(context, resource, alarms);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.alarm_item_layout, parent, false);
+        }
+
+        Alarm alarm = getItem(position);
+
+        if (alarm != null) {
+            TextView timeTextView = convertView.findViewById(R.id.alarmTimeTextView);
+            TextView amPmTextView = convertView.findViewById(R.id.alarmDaysTextView);
+            Button activateButton = convertView.findViewById(R.id.activateButton);
+
+            // Format LocalTime to display in TextView
+            timeTextView.setText(alarm.getTime().toString());
+
+            // Display AM/PM based on the dayTime variable
+            amPmTextView.setText(alarm.getDayTime());
+
+            // Set the button state based on the alarm's activation status
+            activateButton.setText(alarm.isStatut() ? "Deactivate" : "Activate");
+
+            // Handle button click to toggle activation status
+            activateButton.setOnClickListener(view -> {
+                // Toggle the activation status of the alarm
+                alarm.setStatut(!alarm.isStatut());
+                // Update the button text accordingly
+                activateButton.setText(alarm.isStatut() ? "Deactivate" : "Activate");
+            });
+        }
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/alarm/MainActivity.java
+++ app/src/main/java/com/example/alarm/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.alarm;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private List<Alarm> alarmList;
+    private AlarmAdapter alarmAdapter;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.alarmlist);
+        alarmList = new ArrayList<>();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            alarmList.add(new Alarm(LocalTime.parse("08:00"), "AM", true));
+            alarmList.add(new Alarm(LocalTime.parse("19:30"), "PM", false));
+        }
+        alarmAdapter = new AlarmAdapter(this, R.layout.alarm_item_layout, alarmList);
+        ListView alarmListView = findViewById(R.id.alarmListView);
+        alarmListView.setAdapter(alarmAdapter);
+
+
+        // Handle Add Alarm button click
+        Button addAlarmButton = findViewById(R.id.addAlarmButton);
+        addAlarmButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
     }
 }
